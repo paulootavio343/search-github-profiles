e5b9d8068cca84c29d293f5344d3d154ac0495c8
Paulo Otávio <92699268+paulootavio343@users.noreply.github.com>
2022-11-14 00:06:43 -0300

    Add project base files
    Create HTML and CSS.
    Use Axios to fetch data from the API.

---
+++ assets/js/code.js
@@ -0,0 +1,139 @@
+const APIURL = 'https://api.github.com/users/'
+const form = document.getElementById('src-form');
+const search = document.getElementById('search');
+const card = document.getElementById('card-container');
+const reposContainer = document.getElementById('repos');
+
+async function getUser(username) {
+    try {
+        const { data } = await axios(APIURL + username);
+        createUserCard(data);
+    } catch (error) {
+        if (error.response.status == 404) {
+            createErrorCard('No profile with this username.');
+        } else {
+            createErrorCard('Error fetching data from the API.');
+        }
+    }
+}
+
+async function getRepos(username) {
+    try {
+        const { data } = await axios(APIURL + username + '/repos');
+        createRepoCard(data);
+    } catch (error) {
+        if (error.response.status == 404) {
+            createErrorCard('No repository found.');
+        } else {
+            createErrorCard('Error fetching data from the API.');
+        }
+    }
+}
+
+function createRepoCard(repos) {
+    reposContainer.innerHTML = '';
+    repos.forEach(repo => {
+        const cardHTML = `
+        <div class="repo">
+            <div class="name">
+                <h1>${repo.name}</h1>
+                <span class="visibility">${repo.visibility}</span>
+            </div>
+    
+            <div class="description">
+                <p>${repo.description}</p>
+            </div>
+    
+            <div class="info">
+                <div class="topics">
+                    <ul>
+                        <li>${repo.topics[0]}</li>
+                    </ul>
+                </div>
+    
+                <div class="info-2">
+                    <div class="programing-language">
+                        <img src="assets/icons/code.svg" alt="Programing language">
+                        ${repo.language}
+                    </div>
+                    <div class="license">
+                        <img src="assets/icons/balance.svg" alt="License">
+                        ${repo.license.name}
+                    </div>
+                    <div class="updated">
+                        <img src="assets/icons/schedule.svg" alt="Updated">
+                        ${repo.updated_at}
+                    </div>
+                    <div class="stars">
+                        <img src="assets/icons/star.svg" alt="Stars">
+                        ${repo.stargazers_count} stars
+                    </div>
+                    <div class="watching">
+                        <img src="assets/icons/visibility.svg" alt="Watching">
+                        ${repo.watchers} watching
+                    </div>
+                    <div class="forks">
+                        <img src="assets/icons/share.svg" alt="Forks">
+                        ${repo.forks} forks
+                    </div>
+                </div>
+            </div>
+        </div>
+        `
+
+        reposContainer.innerHTML += cardHTML;
+    });
+}
+
+function createUserCard(user) {
+    const cardHTML = `
+    <div class="card">
+        <div class="profile-picture">
+            <img src="${user.avatar_url}" alt="">
+        </div>
+
+        <div class="info">
+            <div class="name">
+                <h1>${user.name}</h1>
+                <span>${user.login}</span>
+            </div>
+
+            <div class="description">
+                <p>${user.bio}</p>
+            </div>
+
+            <div class="numbers">
+                <ul>
+                    <li class="followers">${user.followers} followers</li>
+                    <li class="following">${user.following} following</li>
+                    <li class="repos">${user.public_repos} repos</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    `
+
+    card.innerHTML = cardHTML;
+}
+
+function createErrorCard(message) {
+    const cardHTML = `
+    <div class="card">
+        <h1>${message}</h1>
+    </div>
+    `
+
+    card.innerHTML = cardHTML;
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const user = search.value;
+
+    if (user) {
+        getUser(user);
+        getRepos(user);
+        search.value = '';
+    }
+})
